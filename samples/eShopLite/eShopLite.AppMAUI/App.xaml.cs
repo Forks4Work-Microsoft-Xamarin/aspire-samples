8af7feb389d15cd6aaf5f198ae370aad2f0b3bfb
moljac <mcvjetko@holisticware.net>
2024-01-20 19:29:10 +0100

    maui

---
+++ samples/eShopLite/eShopLite.AppMAUI/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace eShopLite.AppMAUI;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ samples/eShopLite/eShopLite.AppMAUI/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace eShopLite.AppMAUI;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
